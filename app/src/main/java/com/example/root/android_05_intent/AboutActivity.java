fe1a03433468f74dfa97ca9ef2e53b00b415daad
MAlex <alexmaximovj@gmail.com>
2015-08-05 23:39:13 +0300

    Initial commit

---
+++ app/src/main/java/com/example/root/android_05_intent/AboutActivity.java
@@ -0,0 +1,15 @@
+package com.example.root.android_05_intent;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by root on 05.08.15.
+ */
+public class AboutActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
